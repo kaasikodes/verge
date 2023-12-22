308b02ad0025f418e88df0f2c9805eac97ddd558
kaasikodes <thisbroisfresh@gmail.com>
2023-12-22 19:42:01 +0100

    chore: adds resuable assets

---
+++ src/assets/index.ts
@@ -0,0 +1,47 @@
+import LightLogo from "./logo-light.svg";
+import PlusIcon from "./Plus.svg";
+import CalenderIcon from "./Calendar.svg";
+import NotificationIcon from "./Notification.svg";
+import UserIcon from "./User.svg";
+import SearchIcon from "./Search.svg";
+import SettingIcon from "./Setting.svg";
+import HomeIcon from "./Home.svg";
+import BlueHomeIcon from "./BlueHome.svg";
+import BlueProfileIcon from "./BlueProfile.svg";
+import RedUpIcon from "./RedUp.svg";
+import GreenUpIcon from "./GreenUp.svg";
+import WalletIcon from "./Wallet.svg";
+import ProfileIcon from "./Profile.svg";
+import NoneIcon from "./None.svg";
+import BulbIcon from "./Bulb.svg";
+import LockIcon from "./Lock.svg";
+import CloseIcon from "./Close.svg";
+import BookmarkIcon from "./Bookmark.svg";
+import LocationIcon from "./Location.svg";
+import WierdHandIcon from "./WierdHand.svg";
+import CalenderOutlinedIcon from "./CalendarOutlined.svg";
+
+export {
+  LockIcon,
+  CloseIcon,
+  BulbIcon,
+  BookmarkIcon,
+  NoneIcon,
+  BlueHomeIcon,
+  BlueProfileIcon,
+  RedUpIcon,
+  GreenUpIcon,
+  WierdHandIcon,
+  SettingIcon,
+  HomeIcon,
+  WalletIcon,
+  ProfileIcon,
+  LocationIcon,
+  CalenderOutlinedIcon,
+  LightLogo,
+  PlusIcon,
+  CalenderIcon,
+  NotificationIcon,
+  UserIcon,
+  SearchIcon,
+};
