3e928542a3888659e3a518c4508d4c50d58341fa
kaasikodes <thisbroisfresh@gmail.com>
2023-12-22 23:43:49 +0100

    chore: corrects listing view card proportions

--- src/modules/dashboard/overviewInsights/ListingOverviewCard.tsx
+++ src/modules/dashboard/overviewInsights/ListingOverviewCard.tsx
@@ -37,30 +37,32 @@ const ListingOverviewCard: React.FC<TProps> = ({
           </button>
         )}
       </CardHeader>
-      <CardBody className="grid grid-cols-2 gap-4">
-        <div className="flex flex-col gap-4">
-          <h6 className="border-b pb-2">Most Viewed</h6>
+      <CardBody className="grid grid-cols-2 gap-12">
+        <div className="flex flex-col ">
+          <h6 className="border-b-2 pb-2 font-medium">Most Viewed</h6>
           <CountInfo
             borderClassName="border border-[#12D8A0]"
             image={data?.mostViewed?.name}
             name={data?.mostViewed?.name}
           />
         </div>
-        <div className="flex flex-col gap-4">
-          <h6 className="border-b pb-2">Most Shared</h6>
+        <div className="flex flex-col">
+          <h6 className="border-b-2 pb-2 font-medium">Most Shared</h6>
           <CountInfo
             borderClassName="border border-[#4545FE]"
             name={data?.mostShared?.name}
             image={data?.mostShared?.image}
           />
         </div>
-        <div className="col-span-2 bg-[#F5F5F5] rounded-md px-4 py-2 flex items-center gap-4">
-          <div className="flex-1">
+        <div className="col-span-2 bg-[#F5F5F5] rounded-md px-4 py-3 flex items-center gap-5">
+          <div className="">
             <CountInfo
               borderClassName="border border-[#FF9103]"
               name={data?.mostWatchListed?.name}
               highlight="Most Watchlisted"
               image={data?.mostWatchListed?.image}
+              imageHeight={40}
+              imageWidth={40}
             />
           </div>
           <div className="flex-1">
@@ -76,11 +78,13 @@ const WatchlistCount: React.FC<{ count?: number }> = ({ count = 0 }) => {
   return (
     <Card>
       <CardBody className="flex flex-col gap-4">
-        <Heading size="lg" className="flex justify-between">
-          <span>{count}</span>
-          <Image src={BookmarkIcon} alt="bookmark" height={20} width={20} />
-        </Heading>
-        <p>Number of watchlists</p>
+        <div className="flex justify-between">
+          <span className="text-4xl font-normal">{count}</span>
+          <Image src={BookmarkIcon} alt="bookmark" height={25} width={25} />
+        </div>
+        <p className="text-sm text-[#3D3D3D] font-medium">
+          Number of watchlists
+        </p>
       </CardBody>
     </Card>
   );
@@ -90,17 +94,31 @@ const CountInfo: React.FC<{
   name?: string;
   borderClassName?: string;
   highlight?: string;
-}> = ({ image = NoneIcon, name = "None", borderClassName = "", highlight }) => {
+  imageHeight?: number;
+  imageWidth?: number;
+}> = ({
+  image = NoneIcon,
+  name = "None",
+  borderClassName = "",
+  highlight,
+  imageHeight = 56,
+  imageWidth = 56,
+}) => {
   return (
-    <div className="flex flex-col gap-4">
-      <p className="font-semibold text-sm">{highlight}</p>
+    <div className="flex flex-col gap-2">
+      <p className="font-medium text-sm mb-2">{highlight}</p>
       <div className={`flex gap-4 `}>
         <div
           className={`px-4 py-4 flex items-center justify-center rounded-md ${borderClassName}`}
         >
-          <Image src={image} width={56} height={56} alt={name} />
+          <Image
+            src={image}
+            width={imageWidth}
+            height={imageHeight}
+            alt={name}
+          />
         </div>
-        <p className="capitalize font-bold text-xl">{name}</p>
+        <p className="capitalize font-bold text-lg">{name}</p>
       </div>
     </div>
   );
